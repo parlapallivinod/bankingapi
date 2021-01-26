1ec4e74373a90331ac7264071da6eef66abd48ea
vinod <vinod@r081247.rgukt.in>
2021-01-26 13:43:51 +0530

    2020/01/26

--- src/main/java/in/rgukt/r081247/bankingapi/config/JpaConfiguration.java
+++ src/main/java/in/rgukt/r081247/bankingapi/config/JpaConfiguration.java
@@ -6,12 +6,48 @@
 
 package in.rgukt.r081247.bankingapi.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "in.rgukt.r081247.bankingapi.repository")
 public class JpaConfiguration {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("banking.datasource")
+    public DataSourceProperties bankingDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("banking.datasource.hikari")
+    public HikariDataSource bankingDataSource() {
+        return bankingDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    @ConfigurationProperties("user.datasource")
+    public DataSourceProperties userDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("user.datasource.hikari")
+    public HikariDataSource userDataSource() {
+        return userDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
 }
--- src/test/java/in/rgukt/r081247/bankingapi/BankingApiApplicationTests.java
+++ src/test/java/in/rgukt/r081247/bankingapi/BankingApiApplicationTests.java
@@ -1,5 +1,6 @@
 package in.rgukt.r081247.bankingapi;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 

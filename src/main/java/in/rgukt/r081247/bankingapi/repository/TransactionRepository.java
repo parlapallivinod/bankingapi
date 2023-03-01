5f95b517f00402f9a5b764241862895331826ec3
vinod <vinod@r081247.rgukt.in>
2023-03-01 21:41:11 +0530

    20230301

--- src/main/java/in/rgukt/r081247/bankingapi/repository/TransactionRepository.java
+++ src/main/java/in/rgukt/r081247/bankingapi/repository/TransactionRepository.java
@@ -14,6 +14,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, String> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     Page<Transaction> findByFromUserOrToUser(User fromUser, User toUser, Pageable pageable);
 }
